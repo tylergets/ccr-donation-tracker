2ed56b9508a10c3b6b1d0b2861b1288614f367b6
Tyler Getsay <tylergetsay@gmail.com>
2024-09-04 17:26:06 -0400

    chore: fix times

--- nuxt/server/api/export.post.ts
+++ nuxt/server/api/export.post.ts
@@ -6,13 +6,19 @@ export default eventHandler(async (event) => {
 
     const body = await readBody(event);
 
+    function subtractHours(date: any, hours: any) {
+        const dateCopy = new Date(date);
+        dateCopy.setHours(dateCopy.getHours() - hours);
+        return date;
+    }
+
     const donations = await useDrizzle()
         .query
         .donations
         .findMany({
             where: (donations, { between }) => {
-                const start = new Date(body.start);
-                const end = new Date(body.end);
+                const start = subtractHours(new Date(body.start), 5);
+                const end = subtractHours(new Date(body.end), 5);
                 if (body.start && body.end) {
                     return between(donations.createdAt, start, end);
                 } else if (body.start) {
